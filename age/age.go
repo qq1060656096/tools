022f0f559804b9973dff629884f9ba92e8fb59a6
赵思贵 <1060656096@qq.com>
2020-05-17 10:39:08 +0800

    aaa: age完成

---
+++ age/age.go
@@ -0,0 +1,33 @@
+package age
+
+import (
+	"time"
+)
+
+// At 获取特定时间之间的年龄
+func At(birthDate time.Time, nowDate time.Time) int {
+	age := nowDate.Year() - birthDate.Year()
+	if nowDate.Month() <= birthDate.Month() && nowDate.Day() < birthDate.Day(){
+		age -= 1
+	}
+	return age
+}
+
+// Get 根据出生日期获取年龄
+func Get(birthdayTime time.Time) int {
+	return At(birthdayTime, time.Now())
+}
+
+// IsLeapYear 检测日期是否是闰年
+func IsLeapYear(date time.Time) bool {
+	y := date.Year()
+	switch {
+	case y % 400 == 0:
+		return true
+	case y % 100 == 0:
+		return true
+	case y % 4 == 0:
+		return true
+	}
+	return false
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/qq1060656096/tools
+
+go 1.14
+
+require github.com/stretchr/testify v1.5.1
